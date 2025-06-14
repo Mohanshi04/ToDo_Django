6c5b540ddda089a2f8877814c838dc27bcee2393
Mohanshi <jainmohanshi@gmail.com>
2025-06-14 18:20:28 +0530

    modified settings.py

--- ToDo_Django/settings.py
+++ ToDo_Django/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +43,7 @@ EMAIL_HOST_PASSWORD=os.environ['EMAIL_HOST_PASSWORD']
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m$h#7x94-#52c6m7mte4qqfb+weyg#7h5vribusii=^k7&wo!z'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
